c5ae1454bdd315417c23f5477b1a8a3070793693
vitor morais <vitoramorais@hotmail.com>
2022-03-09 16:36:07 -0300

    questions e answers models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_191608) do
+ActiveRecord::Schema.define(version: 2022_03_09_193453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "score"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "deals", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -22,6 +32,12 @@ ActiveRecord::Schema.define(version: 2022_03_09_191608) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_deals_on_room_id"
     t.index ["user_id"], name: "index_deals_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "question_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -59,6 +75,8 @@ ActiveRecord::Schema.define(version: 2022_03_09_191608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "deals", "rooms"
   add_foreign_key "deals", "users"
 end
