cc7ce792887c17dbaaffe07416d4ccc2194901c1
Juliana Foschiera <julifoschiera@gmail.com>
2022-03-09 16:54:44 -0300

    remove status from room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_181359) do
+ActiveRecord::Schema.define(version: 2022_03_09_195219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2022_03_09_181359) do
     t.string "utilities"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status"
     t.boolean "heart"
   end
 
