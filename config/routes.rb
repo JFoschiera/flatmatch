ba3233584dd0d7198ab2f4c97c21932cf99b7c45
Juliana Foschiera <julifoschiera@gmail.com>
2022-03-10 16:02:48 -0300

    created controler for user to log in with facebook plus a few things more

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users,
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+
+
   root to: 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :rooms do
