df61a9b65de962aee3d7e174384e5761830adf88
vitor morais <vitoramorais@hotmail.com>
2022-03-10 11:47:22 -0300

    Insert Map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_193453) do
+ActiveRecord::Schema.define(version: 2022_03_10_140109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2022_03_09_193453) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
     t.boolean "heart"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
