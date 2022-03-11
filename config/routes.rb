eae105169c62e71b0e026586326d87141492efc8
Leonardo Reina <leo.reina01@gmail.com>
2022-03-11 13:36:39 -0300

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
              controllers: { 
                omniauth_callbacks: 'users/omniauth_callbacks',
               }
+  devise_scope :user do
+    get 'about', to: 'devise/sessions#about'
+  end
+
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
