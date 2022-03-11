707e149e7335f070d4330d57a2b8364cba883615
Leonardo Reina <leo.reina01@gmail.com>
2022-03-11 12:27:11 -0300

    home page html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users,
-             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+             controllers: { 
+               omniauth_callbacks: 'users/omniauth_callbacks',
+              }
 
-  root to: 'page#home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :rooms do
     resources :matchs
