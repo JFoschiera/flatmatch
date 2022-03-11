eb31dce9693ba47767f3bc9ba48c606b4ef27db8
Juliana Foschiera <julifoschiera@gmail.com>
2022-03-11 16:11:03 -0300

    removed phone and about from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_184133) do
+ActiveRecord::Schema.define(version: 2022_03_11_191012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,8 +90,6 @@ ActiveRecord::Schema.define(version: 2022_03_11_184133) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "phone"
-    t.string "about"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
