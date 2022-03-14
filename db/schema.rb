5a70f4a29991132c36f01f860eed32fa2ed2ce0c
Juliana Foschiera <julifoschiera@gmail.com>
2022-03-14 14:00:07 -0300

    added question_description to questions table. Solved display of questions on answers_new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_191012) do
+ActiveRecord::Schema.define(version: 2022_03_14_155552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2022_03_11_191012) do
     t.text "question_description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "answer_description"
   end
 
   create_table "rooms", force: :cascade do |t|
