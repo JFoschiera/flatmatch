0a60b6beeccd58bf204aef9d405fd481527fb936
vitor morais <vitoramorais@hotmail.com>
2022-03-16 12:10:37 -0300

    dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar
   has_many :answers
+  has_many :rooms, dependent: :destroy
   has_one :about
   has_one :phone
   has_many :users1, class_name: "Compatibilities", foreign_key: "user1_id", dependent: :destroy
