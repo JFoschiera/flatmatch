bebf21bae11732d8de4ba16a3efb50c1e511cda0
vitor morais <vitoramorais@hotmail.com>
2022-03-16 16:43:43 -0300

    edit-answer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :avatar
-  has_many :answers
+  has_many :answers, dependent: :destroy
   has_many :likes
   has_many :rooms, dependent: :destroy
   has_one :about
